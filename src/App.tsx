7b768b175a948309a57da7fa08c5172bc2e1d68f
Will-Andrade <willian.santos.andra@outlook.com>
2023-02-17 21:53:11 -0400

    feat: makes an area droppable and adds listeners to handle dragged items into it

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,37 @@ import { useState } from 'react';
 import classes from './App.module.css';
 
 function App() {
-  const [addedItems, setAddedItems] = useState<string[]>();
+  const [addedItems, setAddedItems] = useState<string[]>([]);
 
   const dragStartHandler = (e: React.DragEvent<HTMLLIElement>) => {
     e.dataTransfer.setData("text/plain", String(e.currentTarget.textContent));
-    console.log(e.dataTransfer.getData("text"));
   };
 
-  const onDragOverHandler = (e: React.DragEvent) => {};
-  const onDropHandler = (e: React.DragEvent) => {}
+  const onDragOverHandler = (e: React.DragEvent) => {
+    e.preventDefault();
+  };
+
+  const onDropHandler = (e: React.DragEvent) => {
+    const droppedEl = e.dataTransfer.getData("text");
+    setAddedItems([...addedItems, droppedEl]);
+  }
 
   return (<main className={classes['app-container']}>
     <ul className={classes.draggable}>
-      <li className={classes.item} onDragStart={dragStartHandler}draggable>Item A</li>
-      <li className={classes.item} onDragStart={dragStartHandler}draggable>Item B</li>
-      <li className={classes.item} onDragStart={dragStartHandler}draggable>Item C</li>
-      <li className={classes.item} onDragStart={dragStartHandler}draggable>Item D</li>
+      <li className={classes.item} onDragStart={dragStartHandler} draggable>Item A</li>
+      <li className={classes.item} onDragStart={dragStartHandler} draggable>Item B</li>
+      <li className={classes.item} onDragStart={dragStartHandler} draggable>Item C</li>
+      <li className={classes.item} onDragStart={dragStartHandler} draggable>Item D</li>
     </ul>
 
-    <ul className={classes.droppable} onDragOver={onDragOverHandler} onDrop={onDropHandler}>
-      {addedItems}
+    <ul 
+      className={classes.droppable} 
+      onDrop={onDropHandler}
+      onDragOver={onDragOverHandler}
+    >
+      {addedItems.map((item, index) => (
+        <li key={`${item}-${index}`} className={classes.item} draggable>{item}</li>
+      ))}
     </ul>
   </main>);
 }
