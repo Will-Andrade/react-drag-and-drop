23a29df1ae8ba1a6386ca5a0365b013ee23efe7a
Will-Andrade <willian.santos.andra@outlook.com>
2023-02-17 22:07:24 -0400

    feat: adds the functionality of returning item to past block

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import { useState } from 'react';
 import classes from './App.module.css';
 
+type FetchedItem = {
+  id: string;
+  text: string;
+};
+
+const mockItems = [
+  { id: 'itemA', text: "Item A" },
+  { id: 'itemB', text: "item B" },
+  { id: 'itemC', text: "item C" },
+  { id: 'itemD', text: "item D" }
+];
+
 function App() {
+  const [fetchedItems, setFetchedItems] = useState<FetchedItem[]>(mockItems);
   const [addedItems, setAddedItems] = useState<string[]>([]);
 
   const dragStartHandler = (e: React.DragEvent<HTMLLIElement>) => {
@@ -14,15 +27,30 @@ function App() {
 
   const onDropHandler = (e: React.DragEvent) => {
     const droppedEl = e.dataTransfer.getData("text");
+
     setAddedItems([...addedItems, droppedEl]);
-  }
+    setFetchedItems(fetchedItems.filter(item => item.text !== droppedEl));
+  };
+
+  const onReturnItemHandler = (e: React.DragEvent) => {
+    const droppedEl = e.dataTransfer.getData("text");
+
+    setAddedItems(addedItems.filter(addedItem => addedItem !== droppedEl));
+    setFetchedItems(prevItems => ([...prevItems, { id: droppedEl, text: droppedEl }]));
+  };
 
   return (<main className={classes['app-container']}>
-    <ul className={classes.draggable}>
-      <li className={classes.item} onDragStart={dragStartHandler} draggable>Item A</li>
-      <li className={classes.item} onDragStart={dragStartHandler} draggable>Item B</li>
-      <li className={classes.item} onDragStart={dragStartHandler} draggable>Item C</li>
-      <li className={classes.item} onDragStart={dragStartHandler} draggable>Item D</li>
+    <ul className={classes.draggable} onDrop={onReturnItemHandler} onDragOver={onDragOverHandler}>
+      {fetchedItems.map((item) => (
+        <li 
+          key={item.id}
+          className={classes.item}
+          onDragStart={dragStartHandler}
+          draggable
+        >
+          {item.text}
+        </li>
+      ))}
     </ul>
 
     <ul 
@@ -31,7 +59,14 @@ function App() {
       onDragOver={onDragOverHandler}
     >
       {addedItems.map((item, index) => (
-        <li key={`${item}-${index}`} className={classes.item} draggable>{item}</li>
+        <li 
+          key={`${item}-${index}`} 
+          className={classes.item} 
+          onDragStart={dragStartHandler}
+          draggable
+        >
+          {item}
+        </li>
       ))}
     </ul>
   </main>);
