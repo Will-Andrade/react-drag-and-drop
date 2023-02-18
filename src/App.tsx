f5030ff46e21f91a4d5474f7aab78d14efba0b06
Will-Andrade <willian.santos.andra@outlook.com>
2023-02-17 20:45:17 -0400

    feat: make item "draggable" & attached listener

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import classes from './App.module.css';
 
 function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+  const [addedItems, setAddedItems] = useState<string[]>();
+
+  const dragStartHandler = (e: React.DragEvent<HTMLLIElement>) => {
+    e.dataTransfer.setData("text/plain", String(e.currentTarget.textContent));
+    console.log(e.dataTransfer.getData("text"));
+  };
+
+  const onDragOverHandler = (e: React.DragEvent) => {};
+  const onDropHandler = (e: React.DragEvent) => {}
+
+  return (<main className={classes['app-container']}>
+    <ul className={classes.draggable}>
+      <li className={classes.item} onDragStart={dragStartHandler}draggable>Item A</li>
+      <li className={classes.item} onDragStart={dragStartHandler}draggable>Item B</li>
+      <li className={classes.item} onDragStart={dragStartHandler}draggable>Item C</li>
+      <li className={classes.item} onDragStart={dragStartHandler}draggable>Item D</li>
+    </ul>
+
+    <ul className={classes.droppable} onDragOver={onDragOverHandler} onDrop={onDropHandler}>
+      {addedItems}
+    </ul>
+  </main>);
 }
 
 export default App;
