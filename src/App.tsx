162cb6edc35fb8e8cd958876f1da09c0fa70746a
Willian Andrade <willian.santos.andra@outlook.com>
2023-02-17 22:14:26 -0400

    Feat/make area droppable (#8)
    * feat: makes an area droppable and adds listeners to handle dragged items into it
    
    * feat: adds the functionality of returning item to past block
    
    * feat: adds demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,73 @@
 import { useState } from 'react';
 import classes from './App.module.css';
 
+type FetchedItem = {
+  id: string;
+  text: string;
+};
+
+const mockItems = [
+  { id: 'itemA', text: "Item A" },
+  { id: 'itemB', text: "item B" },
+  { id: 'itemC', text: "item C" },
+  { id: 'itemD', text: "item D" }
+];
+
 function App() {
-  const [addedItems, setAddedItems] = useState<string[]>();
+  const [fetchedItems, setFetchedItems] = useState<FetchedItem[]>(mockItems);
+  const [addedItems, setAddedItems] = useState<string[]>([]);
 
   const dragStartHandler = (e: React.DragEvent<HTMLLIElement>) => {
     e.dataTransfer.setData("text/plain", String(e.currentTarget.textContent));
-    console.log(e.dataTransfer.getData("text"));
   };
 
-  const onDragOverHandler = (e: React.DragEvent) => {};
-  const onDropHandler = (e: React.DragEvent) => {}
+  const onDragOverHandler = (e: React.DragEvent) => {
+    e.preventDefault();
+  };
+
+  const onDropHandler = (e: React.DragEvent) => {
+    const droppedEl = e.dataTransfer.getData("text");
+
+    setAddedItems([...addedItems, droppedEl]);
+    setFetchedItems(fetchedItems.filter(item => item.text !== droppedEl));
+  };
+
+  const onReturnItemHandler = (e: React.DragEvent) => {
+    const droppedEl = e.dataTransfer.getData("text");
+
+    setAddedItems(addedItems.filter(addedItem => addedItem !== droppedEl));
+    setFetchedItems(prevItems => ([...prevItems, { id: droppedEl, text: droppedEl }]));
+  };
 
   return (<main className={classes['app-container']}>
-    <ul className={classes.draggable}>
-      <li className={classes.item} onDragStart={dragStartHandler}draggable>Item A</li>
-      <li className={classes.item} onDragStart={dragStartHandler}draggable>Item B</li>
-      <li className={classes.item} onDragStart={dragStartHandler}draggable>Item C</li>
-      <li className={classes.item} onDragStart={dragStartHandler}draggable>Item D</li>
+    <ul className={classes.draggable} onDrop={onReturnItemHandler} onDragOver={onDragOverHandler}>
+      {fetchedItems.map((item) => (
+        <li 
+          key={item.id}
+          className={classes.item}
+          onDragStart={dragStartHandler}
+          draggable
+        >
+          {item.text}
+        </li>
+      ))}
     </ul>
 
-    <ul className={classes.droppable} onDragOver={onDragOverHandler} onDrop={onDropHandler}>
-      {addedItems}
+    <ul 
+      className={classes.droppable} 
+      onDrop={onDropHandler}
+      onDragOver={onDragOverHandler}
+    >
+      {addedItems.map((item, index) => (
+        <li 
+          key={`${item}-${index}`} 
+          className={classes.item} 
+          onDragStart={dragStartHandler}
+          draggable
+        >
+          {item}
+        </li>
+      ))}
     </ul>
   </main>);
 }
